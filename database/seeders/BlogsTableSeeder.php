53c1496009af557531d84ce6c6b8cb5e85903afd
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2024-05-02 15:24:09 +0600

    authentication implemented

--- database/seeders/BlogsTableSeeder.php
+++ database/seeders/BlogsTableSeeder.php
@@ -15,7 +15,9 @@ class BlogsTableSeeder extends Seeder
             'title' => 'The Importance of Regular Exercise',
             'image' => 'jpg',
             'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et justo nec nulla tristique venenatis at nec mi. Nulla facilisi. Suspendisse potenti.',
-            'author_id' => 1
+            'author_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
 
         // Add blog 2
@@ -23,7 +25,9 @@ class BlogsTableSeeder extends Seeder
             'title' => 'Healthy Eating Habits for a Better Life',
             'image' => 'jpg',
             'content' => 'Sed ullamcorper vehicula tortor, id vestibulum elit finibus vitae. Fusce sed sodales nisi. Curabitur dignissim tristique sapien, vel feugiat mi tempor vitae.',
-            'author_id' => 1
+            'author_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
     }
 }
