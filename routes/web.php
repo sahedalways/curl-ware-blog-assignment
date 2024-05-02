8e7d98c38b952713770ba3ab54eda645d3415a88
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2024-05-02 16:05:49 +0600

    cache clear route added

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Clear route cache:
+Route::get('/clear', function () {
+    Artisan::call('route:cache');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    Artisan::call('cache:clear');
+    return 'Routes cache has been cleared';
+});
+
 
 Auth::routes();
 
